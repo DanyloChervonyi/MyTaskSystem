0a39c1b7e270c7e7ca3b173f28f654e63ce0f6b8
Danila Chervonyi <112933359+DiktatorMira@users.noreply.github.com>
2025-08-09 14:23:36 +0300

    Environment variables are now used in .env

--- server.js
+++ server.js
@@ -1,21 +1,11 @@
-//npm i express
-//npm i nodemon
-//npm run dev
-//npm i typescript @types/node @types/express --save-dev - установить тайпскрипт
-//npx tsc --init - ts.config.json
-//npx tsc --noEmit
 import express from 'express';
+import dotenv from 'dotenv';
 
-const app = express();
+dotenv.config();
+const app = express(), PORT = process.env.PORT || 4200;;
 
-async function main() {
-    app.use(express.json());
-    app.use('/api/twit', (reqest, response) => {
-        response.json({ message: 'success' }).status(200);
-    })
-    app.listen(4200, () => {
-        console.log('Сервер запущен на порту 4200.');
-    })
-}
-
-main();
+app.use(express.json());
+app.use('/api/test', (reqest, response) => {
+    response.json({ message: 'success' }).status(200);
+})
+app.listen(PORT, () => { console.log('Server is running on port ' + PORT); })
